c6483f253432857e025b4c7da63835da62376832
Timon <timon.lukas3@gmail.com>
2019-02-15 14:07:37 +0100

    Beef up that script yoooo

--- instance/make_process.sh
+++ instance/make_process.sh
@@ -7,15 +7,19 @@ then
 	echo 'Arguments:'
 	echo '-h     | Show this message'
 	echo 'NAME   | Name of the project, will drop spaces and special characters'
-	echo '<port> | Port to expose the moodle instance on'
+	echo '<port> | Base port. ${port + 0} is Moodle, ${port + 1} is Camunda, ${port + 2} is Adminer, ${port + 3} is Mailhog.'
 	echo '-r     | Remove the given instance'
 	exit
 fi
 
-export MOODLE_PORT=`echo $2 | grep -o '[0-9]*'`
-export PLUGIN_NAME=`echo $1 | sed 's/[^a-z1-9A-Z]*//g'`
+export PROCESS_NAME=`echo $1 | sed 's/[^a-z1-9A-Z]*//g'`
+export PROCESS_PORT_MOODLE=`echo $2 | grep -o '[0-9]*'`
 
-if docker ps -a | grep -qo swim_${PLUGIN_NAME}_ && [ -z `echo $2 | grep -o '\-r'` ]
+export PROCESS_PORT_CAMUNDA=$((${PROCESS_PORT_MOODLE} + 1))
+export PROCESS_PORT_ADMINER=$((${PROCESS_PORT_MOODLE} + 2))
+export PROCESS_PORT_MAILHOG=$((${PROCESS_PORT_MOODLE} + 3))
+
+if docker ps -a | grep -qo swim_${PROCESS_NAME}_ && [ -z `echo $2 | grep -o '\-r'` ]
 then
 	echo "A process of the same name already exists. Do you wish to erase all data and re-create it? (y/n)"
 	read res
@@ -26,13 +30,13 @@ then
 fi
 
 echo "Stopping possible running docker containers for project..."
-docker ps -a | grep -q swim_${PLUGIN_NAME}_ && docker stop `docker ps -a | grep swim_${PLUGIN_NAME}_ | grep -o ^[a-z0-9]*`
+docker ps -a | grep -q swim_${PROCESS_NAME}_ && docker stop `docker ps -a | grep swim_${PROCESS_NAME}_ | grep -o ^[a-z0-9]*`
 echo "done"
 echo "Removing existing docker containers for project..."
-docker ps -a | grep -q swim_${PLUGIN_NAME}_ && docker rm `docker ps -a | grep swim_${PLUGIN_NAME}_ | grep -o ^[a-z0-9]*`
+docker ps -a | grep -q swim_${PROCESS_NAME}_ && docker rm `docker ps -a | grep swim_${PROCESS_NAME}_ | grep -o ^[a-z0-9]*`
 echo "done"
 echo "Removing lingering persistent volumes for project..."
-docker volume ls | grep -oq "swim${PLUGIN_NAME}_.*$" && docker volume rm `docker volume ls | grep -o "swim${PLUGIN_NAME}_.*$"`
+docker volume ls | grep -oq "swim${PROCESS_NAME}_.*$" && docker volume rm `docker volume ls | grep -o "swim${PROCESS_NAME}_.*$"`
 echo "done"
 
-echo $2 | grep -q '\-r' || docker-compose -p swim_${PLUGIN_NAME} -f moodle-compose.yml up --force-recreate -d
+echo $2 | grep -q '\-r' || docker-compose -p swim_${PROCESS_NAME} -f moodle-compose.yml up --force-recreate -d
