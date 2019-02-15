31abef949bb5b431ac53bd313dc9f86d24ebe2a7
Timon <timon.lukas3@gmail.com>
2019-02-15 14:43:52 +0100

    Automatically create needed Docker network

--- instance/make_all_processes.sh
+++ instance/make_all_processes.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-while IFS='' read p; do ./make_process.sh $(echo $p | grep -o "^[0-9a-z]*") $(echo $p | grep -o "[0-9a-z]*$") -y; done <processes.yml
+docker network create moodle_moodle-net
+while IFS='' read p
+do ./make_process.sh $(echo $p | grep -o "^[0-9a-z]*") $(echo $p | grep -o "[0-9a-z]*$") -y
+done <processes.yml
